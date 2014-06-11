7b6ca716a8f2afc396cfba16148116b4bc202cfa
Maimer <nesnl23@yahoo.com>
2014-06-11 10:43:17 -0400

    Added validations to meetup and comment creation.

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -2,4 +2,8 @@ class Meetup < ActiveRecord::Base
   has_many :users, through: :usermeetups
   has_many :usermeetups
   has_many :comments
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :location, presence: true
 end
